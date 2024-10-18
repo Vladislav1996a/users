d070f875ef807e391f3e69c7f0feb9c1e449ad8c
user <user@users-MacBook-Pro.local>
2024-10-18 11:20:58 +0300

    Create react native CLI

---
+++ App.tsx
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+} from 'react-native';
+
+
+
+function App(): React.JSX.Element {
+ 
+  return (
+    
+   <></>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
